c0cc74a916873a55ddfc4439bab0a81c11159573
JayJay1024 <lygm1062@gmail.com>
2021-09-01 15:28:25 +0800

    chore: hide plo page

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { asyncComponent } from "./components/AsyncComponent";
 const AsyncHome = asyncComponent(import("./page/Home"));
 const AsyncEconomic = asyncComponent(import("./page/Economic"));
-const AsyncPlo = asyncComponent(import("./page/Plo"));
+// const AsyncPlo = asyncComponent(import("./page/Plo"));
 const AsyncNotFound = asyncComponent(import("./page/NotFound"));
 
 export default function router() {
@@ -11,7 +11,7 @@ export default function router() {
     <Switch>
       <Route exact component={AsyncHome} path="/" />
       <Route exact component={AsyncEconomic} path="/economic" />
-      <Route exact component={AsyncPlo} path="/plo" />
+      {/* <Route exact component={AsyncPlo} path="/plo" /> */}
       <Route component={AsyncNotFound} />
     </Switch>
   );
