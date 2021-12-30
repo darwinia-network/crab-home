ca7c7507d9eec9ef5a1825d7362e5a51582b8c47
sxlwar <sxlwar@163.com>
2021-12-30 12:26:13 +0800

    xxx

--- src/page/Grants/index.js
+++ src/page/Grants/index.js
@@ -25,30 +25,35 @@ const applies = [
     title: "Multi-Chain",
     des:
       "You’re bullish on the future of multi-chain strategy and want to execute a multi- or dual-chain deployment.",
+    type: 'primary'
   },
   {
     img: png38,
     title: "Scalability",
     des:
       "You need a lower-cost execution environment but don’t want (or need) to invest time and money into refactoring your Solidity code.",
+    type: 'success'
   },
   {
     img: png39,
     title: "Experimentation",
     des:
       "You want to build and test your application in a growing ecosystem with a strong community and a growing user base.",
+    type: 'warning'
   },
   {
     img: png40,
     title: "Enthusiasm",
     des:
       "You’re excited about the potential of Crab and Kusama, and want to help build the ecosystem.",
+    type: 'dark'
   },
   {
     img: png41,
     title: "Win-Win",
     des:
       "By joining the Crab Grants Program, you will have a greater ability to contribute to Crab’s growth, and your team will have access to great benefits that will boost your project's growth and development.",
+    type: 'primary-desat'
   },
 ];
 
@@ -89,7 +94,7 @@ function Grants() {
         button="primary"
       />
 
-      <section className="pt-4 pt-md-11 welcome" style={{ paddingBottom: 40 }}>
+      <section className="pt-4 pt-md-8 welcome" style={{ paddingBottom: 40 }}>
         <div className="container">
           <div className="row align-items-center">
             <div className="col-12 col-md-5 col-lg-6 order-md-2">
@@ -156,7 +161,111 @@ function Grants() {
         </div>
       </section>
 
-      <section
+      <section className="py-8 py-md-8 bg-gray-200">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-10 col-lg-7 text-center">
+              <h2 className="fw-bold">Purpose and Mission</h2>
+
+              <p className="fs-lg text-muted mb-7 mb-md-9">
+                No matter what you're working on, who you're with, or how many
+                of you there are, Landkit can help.
+              </p>
+            </div>
+          </div>
+          <div className="row gx-4">
+            <div className="col-12 col-lg-6 d-lg-flex mb-4">
+              <div
+                className="card shadow-light-lg overflow-hidden"
+                data-aos="fade-up"
+              >
+                <div className="row">
+                  <div className="col-md-4 position-relative">
+                    <img
+                      src={png46}
+                      className="h-75 position-absolute right-0 mt-7 me-n4"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body py-7 py-md-9 text-center">
+                      <h4 className="fw-bold">Purpose</h4>
+
+                      <p className="text-muted mb-0">
+                        To fund the development of projects and applications
+                        that contribute to the Crab ecosystem and promote
+                        overall network growth.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-lg-6 d-lg-flex mb-4">
+              <div
+                className="card shadow-light-lg overflow-hidden text-center"
+                data-aos="fade-up"
+              >
+                <div className="row">
+                  <div className="col-md-8">
+                    <div className="card-body py-7 py-md-9">
+                      <h4 className="fw-bold">Mission</h4>
+
+                      <p className="text-muted mb-0">
+                        We encourage passionate teams to build innovative,
+                        interconnected projects on Crab and reward use cases
+                        with application scenarios.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <img
+                      src={png47}
+                      className="h-75 position-absolute left-0 mt-7"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div
+                className="card shadow-light-lg overflow-hidden text-center text-lg-start"
+                data-aos="fade-up"
+              >
+                <div className="row">
+                  <div className="col-md-4 mt-6 position-relative">
+                    <img
+                      src={png48}
+                      className={`position-absolute top-middle w-100`}
+                      alt="..."
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body py-7 py-md-9">
+                      <h4 className="fw-bold">Building a community.</h4>
+
+                      <p className="text-muted mb-0">
+                        Landkit is focused on helping foster a community. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                        quis diam tempus tortor egestas blandit. Aliquam erat
+                        volutpat. Phasellus rhoncus pretium molestie. Nulla
+                        facilisi. Etiam sollicitudin lectus ac nisi tincidunt
+                        porttitor. Phasellus eros metus, gravida eu mi ac,
+                        gravida convallis ipsum.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* <section
         className="py-8"
         style={{
           background:
@@ -235,9 +344,9 @@ function Grants() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
-      <section className="py-8">
+      {/* <section className="py-8">
         <div className="row">
           <div
             className={`col-10 col-md-6 offset-1 offset-md-3 ${styles.mainBorder}`}
@@ -272,9 +381,88 @@ function Grants() {
             </div>
           </div>
         </div>
+      </section> */}
+
+      <section className="pt-8 pt-md-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h2 className="mb-8 text-center">Eligible Projects and Ideas</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="card card-row shadow-light-lg mb-6">
+                <div className="row gx-0">
+                  {/* <div className="col-12">
+                    <span className="badge rounded-pill bg-gray-600 badge-float badge-float-outside">
+                      <span className="h6 text-uppercase">Featured</span>
+                    </span>
+                  </div> */}
+                  <a
+                    className="col-12 col-md-6 order-md-2 bg-cover card-img-end"
+                    href="#!"
+                  >
+                    <img src={png43} alt="..." className="img-fluid h-100" />
+
+                    <div className="shape shape-start shape-fluid-y text-white d-none d-md-block">
+                      {/* {{> curves/curve-5}} */}
+                    </div>
+                  </a>
+                  <div className="col-12 col-md-6 order-md-1">
+                    <a className="card-body" href="#!">
+                      <h3>Tooling and Infrastructure</h3>
+
+                      <p className="mb-0 text-muted">
+                        Projects that support the development tools and
+                        infrastructure needed by other teams. These projects
+                        must fill existing tooling gaps from the developer tools
+                        portfolio in Crab. Such as wallets, oracles, and API
+                        services.
+                      </p>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card card-row shadow-light-lg mb-6">
+                <div className="row gx-0">
+                  {/* <div className="col-12">
+                    <span className="badge rounded-pill bg-gray-600 badge-float badge-float-outside">
+                      <span className="h6 text-uppercase">Featured</span>
+                    </span>
+                  </div> */}
+                  <a
+                    className="col-12 col-md-6 bg-cover card-img-start"
+                    href="#!"
+                  >
+                    <img src={png44} alt="..." className="img-fluid h-100" />
+
+                    <div className="shape shape-end shape-fluid-y text-white d-none d-md-block">
+                      {/* {{> curves/curve-4}} */}
+                    </div>
+                  </a>
+                  <div className="col-12 col-md-6">
+                    <a className="card-body" href="#!">
+                      <h3>Ecosystem Projects</h3>
+
+                      <p className="mb-0 text-muted">
+                        Projects that support the ecosystem through a wide range
+                        of use cases, such as Defi, NFT, Dao, identity,
+                        Metaverse games, insurance, and more. This category aims
+                        to promote the Crab ecosystem development by providing
+                        useful services and utility on the network.
+                      </p>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
-      <section className="pt-2 pt-md-4">
+      {/* <section className="pt-2 pt-md-4">
         <div className="container">
           <div className="row">
             <div className="col-12">
@@ -339,9 +527,68 @@ function Grants() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
-      <section className="py-2 py-md-4">
+      <section className="py-8 py-md-8 bg-gradient-light-white border-bottom">
+        <div className="container">
+          <div className="row justify-content-center mb-4">
+            <div className="col-12 col-md-10 col-lg-8 text-center">
+              <h1>Resource for Grants Program</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-6 col-lg-4 d-flex">
+              <div className="card mb-6 mb-lg-0 shadow-light-lg lift lift-lg">
+                <a className="card-img-top" href="#!">
+                  <img src={png49} alt="..." className="img-fluid" />
+                </a>
+
+                <a className="card-body text-center" href="#!">
+                  <h3>Developer Tools</h3>
+
+                  <p className="mb-0 text-muted">
+                    Resources and documentation for your team.
+                  </p>
+                </a>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-4 d-flex">
+              <div className="card mb-6 mb-lg-0 shadow-light-lg lift lift-lg">
+                <a className="card-img-top" href="#!">
+                  <img src={png50} alt="..." className="img-fluid" />
+                </a>
+
+                <a className="card-body text-center" href="#!">
+                  <h3>End-to-End Support</h3>
+
+                  <p className="mb-0 text-muted">
+                    Guidance of technical hurdles and strategic development.
+                  </p>
+                </a>
+              </div>
+            </div>
+
+            <div className="col-12 clo-md-6 col-lg-4 d-flex">
+              <div className="card shadow-light-lg lift lift-lg">
+                <a className="card-img-top" href="#!">
+                  <img src={png51} alt="..." className="img-fluid" />
+                </a>
+
+                <a className="card-body my-auto text-center" href="#!">
+                  <h3 className="mt-auto">Business Development</h3>
+
+                  <p className="mb-0 text-muted">
+                    Recommend well-known venture capital institutions and
+                    industry leaders to promote projects to the next phase.
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <section className="py-2 py-md-4">
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-12 col-md-10 col-lg-8 mb-4 mb-md-7 text-center">
@@ -403,7 +650,7 @@ function Grants() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section className="py-2 py-md-4">
         <div className="container">
@@ -413,11 +660,11 @@ function Grants() {
             </div>
           </div>
           <div className="row">
-            {applies.map(({ img, title, des }) => (
+            {applies.map(({ img, title, des, type }) => (
               <div className={`col-12 col-md-6 col-lg-4`}>
                 <div
-                  className={`card card-border shadow-lg mb-6 mb-md-8 lift lift-lg overflow-hidden ${styles.mainBorder}`}
-                  style={{ borderRadius: 10, height: 400 }}
+                  className={`card card-border border-${type} shadow-lg mb-6 mb-md-8 lift lift-lg overflow-hidden`}
+                  style={{ height: 400 }}
                 >
                   <div className={`card-body text-center `}>
                     <div className="icon-circle text-white mb-10">
@@ -438,12 +685,12 @@ function Grants() {
       <section
         className="py-8"
         style={{
-          background:
-            "linear-gradient(315.52deg, rgba(255,0,80, .1) 0.88%, rgba(112,0,255, .1) 71.7%, rgba(0,39,255,.1) 100%)",
+          background: '#fff'
+            // "linear-gradient(315.52deg, rgba(255,0,80, .1) 0.88%, rgba(112,0,255, .1) 71.7%, rgba(0,39,255,.1) 100%)",
         }}
       >
         <div className="container">
-          <div className="row align-items-center">
+          <div className="row">
             <div className="col-12 col-md-6 col-lg-7">
               <div className="mb-8 mb-md-0">
                 <img
@@ -453,7 +700,7 @@ function Grants() {
                 />
               </div>
             </div>
-            <div className="col-12 col-md-6 col-lg-5">
+            <div className="col-12 col-md-6 col-lg-5 mt-8">
               <h2 className="text-center">How to Apply</h2>
               {appliesAnswers.map(({ title, des }, index) => (
                 <div className="d-flex" key={index}>
