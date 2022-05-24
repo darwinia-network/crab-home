2eb74b4d425bf5f293e97f0fe4d4fdf5975f78b1
JayJay1024 <lygm1062@gmail.com>
2022-05-24 13:58:24 +0800

    refactor: plo header

--- src/page/Plo/index.js
+++ src/page/Plo/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames/bind";
-import Fade from "react-reveal/Fade";
 import Footer from "../../components/footer";
 
 import Header from "./Header";
@@ -19,50 +18,23 @@ const cx = classNames.bind(styles);
 
 const PloPage = () => {
   return (
-    <div className={cx("main")}>
-      <Fade bottom fraction={0.1} duration={1000} distance={"50px"}>
-        <Header />
-      </Fade>
-
-      <Fade bottom fraction={0.2} duration={1500} distance={"50px"}>
-        <Welcome />
-      </Fade>
-
-      <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-        <Work />
-      </Fade>
-
-      <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-        <Contribute />
-      </Fade>
-
-      <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-        <FourCards />
-      </Fade>
-
-      <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-        <Timeline />
-      </Fade>
+    <>
+      <Header />
+      <Welcome />
+      <Work />
+      <Contribute />
+      <FourCards />
+      <Timeline />
 
       <Container fluid className={cx("wrapper")}>
         <img alt="..." src={crabBg} className="d-md-block d-none" />
-        <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-          <FAQs />
-        </Fade>
-
-        <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-          <Waitlist />
-        </Fade>
-
-        <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-          <p className={cx("footer-allright")}>Copyright@2021 Crab Network</p>
-        </Fade>
+        <FAQs />
+        <Waitlist />
+        <p className={cx("footer-allright")}>Copyright@2021 Crab Network</p>
       </Container>
 
-      <Fade bottom fraction={0.2} duration={1000} distance={"50px"}>
-        <Footer classList="bg-dark" />
-      </Fade>
-    </div>
+      <Footer classList="bg-dark" />
+    </>
   );
 };
 
