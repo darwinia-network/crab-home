ce1a688fb0c05819ab4dbb7e415bb823fde74bc6
Ismail Mbarack (Nas) <isunaslabs@users.noreply.github.com>
2022-07-17 09:19:48 +0800

    added separate components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,9 +11,8 @@ import FeatureWrapper from "../../components/FeatureWrapper";
 import CrabTokens from "../../components/CrabTokens";
 import LogosSlider from "../../components/LogosSlider";
 import DeveloperTools from "../../components/DeveloperTools";
-import { useRef, useState } from "react";
-import CustomMarquee from "../../components/CustomMarquee";
-import { CompanyLogo, Link } from "../../data/types";
+import { useRef } from "react";
+import LogosSlider2 from "../../components/LogosSlider2";
 
 const Home = () => {
   const {
@@ -32,9 +31,6 @@ const Home = () => {
   const topSlider = useRef<LogosSlider>(null);
   const bottomSlider = useRef<LogosSlider>(null);
 
-  const [, updateGreetings] = useState("Hello there");
-  const [index, setIndex] = useState(0);
-
   const pauseSlider = () => {
     if (!topSlider.current || !bottomSlider.current) {
       return;
@@ -51,63 +47,11 @@ const Home = () => {
     bottomSlider.current.playSlider();
   };
 
-  const changeGreetings = () => {
-    const newIndex = index + 1;
-    setIndex(() => {
-      return newIndex;
-    });
-    updateGreetings(`Hello there ${newIndex}`);
-  };
-
   return (
     <div>
       <div className={"container inter-block-space-1"}>
-        <button
-          className={"btn"}
-          onClick={() => {
-            changeGreetings();
-          }}
-        >
-          Update
-        </button>
-        <CustomMarquee
-          initialTranslateRatio={0}
-          shouldDuplicate={true}
-          style={{ width: "100%" }}
-          className={"hello-world"}
-        >
-          <div
-            className={"flex"}
-            onClick={() => {
-              console.log("you clicked up====");
-            }}
-          >
-            {companySlider.top.map((item, index) => {
-              return createASlider(item, index, (link) => {
-                console.log(link);
-              });
-            })}
-          </div>
-        </CustomMarquee>
-        <CustomMarquee
-          initialTranslateRatio={0}
-          shouldDuplicate={true}
-          style={{ width: "100%" }}
-          className={"hello-world"}
-        >
-          <div
-            className={"flex"}
-            onClick={() => {
-              console.log("you clicked up====");
-            }}
-          >
-            {companySlider.bottom.map((item, index) => {
-              return createASlider(item, index, (link) => {
-                console.log(link);
-              });
-            })}
-          </div>
-        </CustomMarquee>
+        <LogosSlider2 data={companySlider.top} />
+        <LogosSlider2 data={companySlider.bottom} />
       </div>
       <Hero page={"HOME"} data={heroData} />
       <div className={"container inter-block-space-1"}>
@@ -145,29 +89,6 @@ const Home = () => {
         <DeveloperTools data={developerTools} />
       </div>
       <Footer data={footerData} />
-    </div>
-  );
-};
-
-const createASlider = (company: CompanyLogo, index: number, clickHandler: (link: Link) => void): JSX.Element => {
-  const random = Math.random();
-  return (
-    <div
-      className={"cursor-pointer w-[27.56%] lg:w-[18.61%] max-w-[268px]"}
-      key={`${index}-${random}`}
-      onClick={() => {
-        clickHandler(company.link);
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          textAlign: "center",
-        }}
-      >
-        <img className={"w-full"} src={company.logo} alt="image" />
-      </div>
     </div>
   );
 };
